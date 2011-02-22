d379a2bb8f08c225ce608e71f163302db3fea719
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-02-22 18:29:38 +0100

    added wordpress

---
+++ wordpress/wp-config.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'test');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'w,NwGXviHv_[ub/#f-P9!Nxl 3cQq2y|n)m$b7W>=9}P0}6Oq>)6/4p?@*tI7nWg');
+define('SECURE_AUTH_KEY',  'z.581EB@UbH]-l0CHbIM&V8M-6;2M*`=Gqa<KB9J{%Z9UV0ubwZ^|X.-10-Jy;tT');
+define('LOGGED_IN_KEY',    '-,l&4rxEKZj)iK3`YzQrRdFj*R we50:,`Ts~jszDUa^]>byNppli5v4|*c]Qc@U');
+define('NONCE_KEY',        'mI;^28-`|wTxe(3/l|I=5)K@VceuA3.JwE5}cuNM%QF7boeReSCxw})WQ_);Hrge');
+define('AUTH_SALT',        'PaIzewaT%xClQWUQ>M9%c~7PX!^ry p(+W4txm1m4W,+0#0|H3t|H=nHMz.*UJ+-');
+define('SECURE_AUTH_SALT', '}Qh4&M92-M7lvf+J`eYrq6tgX?G2i5)#T)%1j%T#[eQ/;+]{y4?Pq+tErghc0@b4');
+define('LOGGED_IN_SALT',   'IIcJK_<Yj<kuQ+vptz-2vY-E,LZ[[_y`@`a3ghE)b_niUzB9.R_H{e7x8bC$/3)W');
+define('NONCE_SALT',       '2FKw@>ja:Cb7~ptq=Lt/dG iZ+:U:257Zc7#*/jZ-LYTlQ{GMzBB&sdQ@~2+0]i5');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress.  A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de.mo to wp-content/languages and set WPLANG to 'de' to enable German
+ * language support.
+ */
+define ('WPLANG', '');
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wordpress/wp-content/plugins/blogroll-links/blogroll-links.php
@@ -0,0 +1,294 @@
+<?php
+  /*
+   Plugin Name: Blogroll Links
+   Plugin URI: http://www.rajiv.com/blog/2008/02/10/blogroll-links/
+   Description: Displays blogroll links on a Page or Post. Insert <code>[blogroll-links categoryslug="blogroll"]</code> to a Page or Post and it will display your blogroll links there.
+   Author: Rajiv Pant
+   Version: 2.2
+   Author URI: http://www.rajiv.com/
+   */
+  
+  
+  /*
+   Blogroll Links is a Wordpress Plugin that displays a list of blogroll links
+   in a Post or Page on your Wordpress Blog.
+   
+   Version 1.1 includes modifications made to the admin panel layout to make it
+   better compliant with the WordPress guidelines. Thanks to @federicobond.
+   
+   Version 2 switches over the tag format to WordPress shortcodes.
+   The old format is still supported for backwards compatibility.
+
+   Copyright (C) 2008-2010 Rajiv Pant
+   
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+      
+   Examples of use:
+   
+   New WordPress shortcode format:
+   
+   [blogroll-links categoryslug="rajiv-web" sortby="link_title"]
+   [blogroll-links categoryslug="people" sortby="link_title" sortorder="desc"]
+
+   Old format (now deprecated) from version 1.0 and 1.1:
+   
+   <!--blogroll-links category-slug="rajiv-web" sort-by="link_title"><!--/blogroll-links-->
+   <!--blogroll-links category-slug="people" sort-by="link_title" sort-order="desc"--><!--/blogroll-links-->
+   
+   */
+
+
+
+  function blogroll_links_handler($atts) {
+  
+//    $attributes = ''; // for debugging
+//    foreach ($atts as $key=>$value) { // for debugging
+//        $attributes .= "$key: $value\n"; // for debugging
+//   } // for debugging
+//    return "Attributes: $attributes<br/>Content: $content"; // for debugging
+
+  
+    global $wpdb, $table_prefix;
+      
+    $attributes = shortcode_atts(array(
+        'categoryslug' => get_option('blogroll_links_default_category_slug'),
+        'sortby'       => get_option('blogroll_links_default_sort_by'),
+        'sortorder'    => get_option('blogroll_links_default_sort_order'),
+        'debug'        => '0',
+    ), $atts);
+
+
+	$category_slug = $attributes['categoryslug'];
+    $sort_by       = $attributes['sortby'];
+    $sort_order    = $attributes['sortorder'];
+    $debug         = $attributes['debug'];
+
+          /*
+           
+           Sample SQL Query:
+           SELECT *
+           FROM wp_links, wp_term_relationships
+           WHERE link_id = object_id
+           AND link_visible = 'Y'
+           AND term_taxonomy_id = (
+           SELECT wp_term_taxonomy.term_taxonomy_id
+           FROM wp_term_taxonomy
+           WHERE term_id = (SELECT DISTINCT term_id
+           FROM wp_terms, wp_term_relationships
+           WHERE slug = 'charity'
+           AND taxonomy = 'link_category'))
+           ORDER BY link_name
+           
+           */
+          
+          $sort_by_str = (strlen($sort_by) > 0) ? "ORDER BY " . $sort_by : '';
+          
+          $sql = "SELECT * " . "FROM $wpdb->links, " . $table_prefix . "term_relationships " .
+          "WHERE link_id = object_id " . "AND link_visible='Y' " . // Skip links marked as not to be visible
+          "AND term_taxonomy_id = ( " . "SELECT " . $table_prefix . "term_taxonomy.term_taxonomy_id " .
+          "FROM " . $table_prefix . "term_taxonomy " . "WHERE term_id = (SELECT DISTINCT term_id " .
+          "FROM " . $table_prefix . "terms, " . $table_prefix . "term_relationships " .
+          "WHERE slug = '" . $category_slug . "' " . "AND taxonomy = 'link_category')) " .
+          $sort_by_str;
+
+          if ($debug == 1) {
+			  $links .= "category-slug=". $category_slug . "\n<br />";
+			  $links .= "sort-order=". $sort_order . "\n<br />";
+			  $links .= "sort-by=". $sort_by . "\n<br />";
+			  $links .= "sql=". $sql . "\n<br />";
+          }
+          
+          $alllinks = $wpdb->get_results($sql);
+          
+          $links .= '<ul>';
+          
+          foreach ($alllinks as $link) {
+              $url = $link->link_url;
+              $name = $link->link_name;
+              $description = (strlen($link->link_description) > 0) ? ' - ' . $link->link_description : '';
+              $rel = (strlen($link->link_rel) > 0) ? ' rel="' . $link->link_rel . '"' : '';
+              $image = (strlen($link->link_image) > 0) ? '<br />' . '<img src="' . $link->link_image . '" border="0"/>' : '';
+              $target = (strlen($link->link_target) > 0) ? ' target="' . $link->link_target . '"' : '';
+              $links .= '<li><a href="' . $url . '"' . $rel . $target . '>' . $name . '</a>' . $description . $image . '</li>';
+          }
+          
+          $links .= '</ul>';
+                
+      return $links;
+  }
+
+
+  
+  
+  // Replaces the <!--blogroll-links--> tag and its contents with the blogroll links
+  // This function supports a previous (now deprecated) syntax
+  function blogroll_links_text($text)
+  {
+      global $wpdb, $table_prefix;
+      
+      // Only perform plugin functionality if post/page contains <!-- show-blogroll-links -->
+      while (preg_match("{<!--blogroll-links\b(.*?)-->.*?<!--/blogroll-links-->}", $text, $matches)) {
+          // to contain the XHTML code that contains the links returned
+          $links = '';
+          
+          
+          $tmp = get_option('blogroll_links_default_category_slug');
+          $category_slug = (strlen($tmp) > 0) ? $tmp : 'blogroll';
+          
+          $tmp = get_option('blogroll_links_default_sort_by');
+          $sort_by = (strlen($tmp) > 0) ? $tmp : 'link_name';
+          
+          $tmp = get_option('blogroll_links_default_sort_order');
+          $sort_order = (strlen($tmp) > 0) ? $tmp : '';
+          
+          $attributes = $matches[1];
+          
+          if (preg_match("{\bcategory-slug\b=\"(.*?)\"}", $attributes, $matches)) {
+              $category_slug = $matches[1];
+          }
+          
+          if (preg_match("{\bsort-by\b=\"(.*?)\"}", $attributes, $matches)) {
+              $sort_by = $matches[1];
+          }
+          
+          if (preg_match("{\bsort-order\b=\"(.*?)\"}", $attributes, $matches)) {
+              $sort_order = $matches[1];
+          }
+          
+          
+          /*
+           
+           Sample SQL Query:
+           SELECT *
+           FROM wp_links, wp_term_relationships
+           WHERE link_id = object_id
+           AND link_visible = 'Y'
+           AND term_taxonomy_id = (
+           SELECT wp_term_taxonomy.term_taxonomy_id
+           FROM wp_term_taxonomy
+           WHERE term_id = (SELECT DISTINCT term_id
+           FROM wp_terms, wp_term_relationships
+           WHERE slug = 'charity'
+           AND taxonomy = 'link_category'))
+           ORDER BY link_name
+           
+           */
+          
+          $sql = "SELECT * " . "FROM $wpdb->links, " . $table_prefix . "term_relationships " . "WHERE link_id = object_id " . "AND link_visible='Y' " . // Skip links marked as not to be visible
+          "AND term_taxonomy_id = ( " . "SELECT " . $table_prefix . "term_taxonomy.term_taxonomy_id " . "FROM " . $table_prefix . "term_taxonomy " . "WHERE term_id = (SELECT DISTINCT term_id " . "FROM " . $table_prefix . "terms, " . $table_prefix . "term_relationships " . "WHERE slug = '" . $category_slug . "' " . "AND taxonomy = 'link_category')) " . "ORDER BY " . $sort_by;
+          
+          //$links .= "attributes=". $attributes . "\n<br />"; // for debugging
+          //$links .= "category_slug=". $category_slug . "\n<br />"; // for debugging
+          //$links .= "sql=". $sql . "\n<br />"; // for debugging
+          
+          
+          $alllinks = $wpdb->get_results($sql);
+          
+          $links .= '<ul>' . "\n";
+          
+          foreach ($alllinks as $link) {
+              $url = $link->link_url;
+              $name = $link->link_name;
+              $description = (strlen($link->link_description) > 0) ? ' - ' . $link->link_description : '';
+              $rel = (strlen($link->link_rel) > 0) ? ' rel="' . $link->link_rel . '"' : '';
+              $image = (strlen($link->link_image) > 0) ? '<img src="' . $link->link_image . '" border="0"/>' : '';
+              $target = (strlen($link->link_target) > 0) ? ' target="' . $link->link_target . '"' : '';
+              $links .= '<li><a href="' . $url . '"' . $rel . $target . '>' . $name . '</a>' . $description . '<br />' . $image . '</li>' . "\n";
+          }
+          
+          $links .= '</ul>' . "\n";
+          
+          // by default preg_replace replaces all, so the 4th paramter is set to 1, to only replace once.
+          $text = preg_replace("{<!--blogroll-links\b.*?-->.*?<!--/blogroll-links-->}", $links, $text, 1);
+      }
+      // end while loop
+      
+      return $text;
+  }
+  // end function blogroll_links_text()
+  
+  
+  
+  
+  
+  // admin menu
+  function blogroll_links_admin()
+  {
+      if (function_exists('add_options_page')) {
+          add_options_page('Blogroll Links', 'Blogroll Links', 1, basename(__FILE__), 'blogroll_links_admin_panel');
+      }
+  }
+  
+  
+  function blogroll_links_admin_panel()
+  {
+      // Add options if first time running
+      add_option('blogroll_links_new_window', 'no', 'Blogroll Links - open links in new window');
+      
+      if (isset($_POST['info_update'])) {
+          // update settings
+          
+          if ($_POST['new-window'] == 'on') {
+              $new = 'yes';
+          } else {
+              $new = 'no';
+          }
+          
+          update_option('blogroll_links_new_window', $new);
+      } else {
+          // load settings from database
+          $new = get_option('blogroll_links_default_category_slug');
+      }
+?>
+<div class=wrap>
+  <form method="post">
+    <h2>Blogroll Links Plugin Options</h2>
+    <h3 class="title">Default Settings</h3>
+    <table class="form-table">
+      <tr valign="top">
+        <th scope="row"><label for="blogroll_links_default_category_slug">Category Slug</label></th>
+        <td><input class="regular-text" type="text" id="blogroll_links_default_category_slug" name="blogroll_links_default_category_slug" value="<?php
+      checked('yes', $new);
+?>" /></td>
+      </tr>
+      <tr valign="top">
+        <th scope="row"><label for="blogroll_links_default_sort_by">Sort-By</label></th>
+        <td><input class="regular-text" type="text" id="blogroll_links_default_sort_by" name="blogroll_links_default_sort_by" value="<?php
+      checked('yes', $new);
+?>" /></td>
+      </tr>
+      <tr valign="top">
+        <th scope="row"><label for="blogroll_links_default_sort_order">Sort Order</label></th>
+        <td><input class="regular-text" type="text" id="blogroll_links_default_sort_order" name="blogroll_links_default_sort_order" value="<?php
+      checked('yes', $new);
+?>" /></td>
+      </tr>
+    </table>
+    <p class="submit">
+      <input type="submit" class="button-primary" name="info_update" value="<?php
+      _e('Save Changes')
+?>" />
+    </p>
+  </form>
+</div>
+<?php
+      } // end function blogroll_links_admin_panel()
+      
+      
+      // hooks
+      add_filter('the_content', 'blogroll_links_text', 2);
+      add_shortcode('blogroll-links', 'blogroll_links_handler');
+      add_action('admin_menu', 'blogroll_links_admin');
+?>
