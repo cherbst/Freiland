812d7d9e9580415b4489f8a837c64c462aec5065
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-02-23 12:30:22 +0100

    added skeleton for smw import plugin

---
+++ wordpress/wp-content/plugins/smw-import.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: SMW-Import
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Imports informations from a SMW into wordpress
+Version: 0.1
+Author: Christoph Herbst
+Author URI: http://URI_Of_The_Plugin_Author
+License: GPL2
+*/
+/*  Copyright 2011  Christoph Herbst  (email : chris.p.herbst@googlemail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+?>
