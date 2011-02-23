a79519e3755ea2021d23cef87fa7e0ef79c274cd
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-02-23 13:03:30 +0100

    added option for smw host

--- wordpress/wp-content/plugins/smw-import.php
+++ wordpress/wp-content/plugins/smw-import.php
@@ -24,4 +24,99 @@ License: GPL2
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+
+// Hook for adding admin menus
+add_action('admin_menu', 'smwimport_add_pages');
+
+// action function for above hook
+function smwimport_add_pages() {
+    $title = 'SMW Import';
+    $slug = 'smwimport';
+    // Add a new submenu under Tools:
+    add_management_page( __($title,'menu-smwimport'), __($title,'menu-smwimport'), 'manage_options',$slug, 'smwimport_tools_page');
+
+  // Add a new submenu under Settings:
+    add_options_page(__($title,'menu-smwimport'), __($title,'menu-smwimport'), 'manage_options', $slug, 'smwimport_settings_page');
+
+}
+
+
+// mt_tools_page() displays the page content for the Test Tools submenu
+function smwimport_tools_page() {
+    //must check that the user has the required capability 
+    if (!current_user_can('manage_options'))
+    {
+      wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+
+
+    echo "<h2>" . __( 'SMW Import', 'menu-smwimport' ) . "</h2>";
+}
+
+
+// smw_import_page() displays the page content for the Test tools submenu
+function smwimport_settings_page() {
+
+    //must check that the user has the required capability 
+    if (!current_user_can('manage_options'))
+    {
+      wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+
+    // variables for the field and option names 
+    $opt_name = 'smwimport_smw_host';
+    $hidden_field_name = 'smwimport_submit_hidden';
+    $data_field_name = 'smwimport_smw_host';
+
+    // Read in existing option value from database
+    $opt_val = get_option( $opt_name );
+
+    // See if the user has posted us some information
+    // If they did, this hidden field will be set to 'Y'
+    if( isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' ) {
+        // Read their posted value
+        $opt_val = $_POST[ $data_field_name ];
+
+        // Save the posted value in the database
+        update_option( $opt_name, $opt_val );
+
+        // Put an settings updated message on the screen
+
+?>
+<div class="updated"><p><strong><?php _e('settings saved.', 'menu-smwimport' ); ?></strong></p></div>
+<?php
+
+    }
+
+    // Now display the settings editing screen
+
+    echo '<div class="wrap">';
+
+    // header
+
+    echo "<h2>" . __( 'SMW Import Settings', 'menu-smwimport' ) . "</h2>";
+
+    // settings form
+    
+    ?>
+
+<form name="form1" method="post" action="">
+<input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
+
+<p><?php _e("SMW Host name:", 'menu-smwimport' ); ?> 
+<input type="text" name="<?php echo $data_field_name; ?>" value="<?php echo $opt_val; ?>" size="20">
+</p><hr />
+
+<p class="submit">
+<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+</p>
+
+</form>
+</div>
+
+<?php
+ 
+}
+
+
 ?>
