35e9f139ea94d906b7799ebec3a4d4a2d9cad9fb
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-03-20 22:59:45 +0100

    twentyten: do not show comment link

--- wordpress/wp-content/themes/twentyten/loop.php
+++ wordpress/wp-content/themes/twentyten/loop.php
@@ -161,7 +161,7 @@
 					</span>
 					<span class="meta-sep">|</span>
 				<?php endif; ?>
-				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
+				<span class="comments-link"><?php if ( comments_open())  comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
 				<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
 			</div><!-- .entry-utility -->
 		</div><!-- #post-## -->
