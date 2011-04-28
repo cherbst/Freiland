9b32605c34b5c1057345e679192ff24e465950c9
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-04-28 15:16:48 +0200

    freiland/category: remove category description

--- wordpress/wp-content/themes/freiland/category.php
+++ wordpress/wp-content/themes/freiland/category.php
@@ -13,9 +13,6 @@ get_header(); ?>
 			<div id="content" role="main">
 
 				<?php
-					$category_description = category_description();
-					if ( ! empty( $category_description ) )
-						echo '<div class="archive-meta">' . $category_description . '</div>';
 
 				/* Run the loop for the category page to output the posts.
 				 * If you want to overload this in a child theme then include a file
