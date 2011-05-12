19fe6617c5db424d288c92676d6b7ba61606277c
rb.magnus <rb.magnus@online.de>
2011-05-12 22:20:47 +0200

    reverted changes in config

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'freiland');
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Lu1sM1gu3l');
+define('DB_PASSWORD', 'test');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
