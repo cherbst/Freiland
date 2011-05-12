9727648175e009dfa05ffd30cabcfb91d8dc98dc
rb.magnus <rb.magnus@online.de>
2011-05-12 22:16:33 +0200

    fixed size layout

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'freiland');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'test');
+define('DB_PASSWORD', 'Lu1sM1gu3l');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
