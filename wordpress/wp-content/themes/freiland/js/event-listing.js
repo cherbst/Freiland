d380a764a5ba705a8343191d7c09b1317aa7afc4
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-05-16 00:22:32 +0200

    js: update calendar when scrolling through events

--- wordpress/wp-content/themes/freiland/js/event-listing.js
+++ wordpress/wp-content/themes/freiland/js/event-listing.js
@@ -5,6 +5,11 @@ jQuery(document).ready(function(){
 	var postreq = 0;
 	var reload = false;
 	var newMonthHref;
+	var scroll = false;
+	var prev_cal = null;
+	var next_cal = null;
+	var next_month_post = jQuery();
+	var prev_month_post = jQuery();
 
 	// filter posts for given category
 	// show 404 if no posts found
@@ -35,9 +40,9 @@ jQuery(document).ready(function(){
 
 	var curCat = getCatId(jQuery('.current-menu-item'));
 
-	var scrollTo = function(elem,duration){
+	var getPostFromCalDay = function(elem){
 		var eventDay = jQuery();
-		var curDay = elem.parent();
+		var curDay = elem;
 		while ( eventDay.length == 0 && curDay.length!=0 ){
 			if ( curDay.hasClass('ec3_eventday') )
 				eventDay = curDay;
@@ -45,29 +50,43 @@ jQuery(document).ready(function(){
 				eventDay = curDay.nextAll('.ec3_eventday').filter(':visible').first();
 			curDay = curDay.parent().next().children().first();
 		}
-		if ( eventDay.length == 0 ) return;
+		if ( eventDay.length == 0 ) return jQuery();
 		eventDay = eventDay.children('a');
 		var id = eventDay.attr('postids');
 		id=id.split(",");
-		if(!id) return false;
+		if(!id) return jQuery();
 		id = id[0].trim();
-		var post = jQuery('#post-'+id);
+		return jQuery('#post-'+id);
+	};
+	
+	var scrollTo = function(elem,duration){
+		if ( duration === false ) return;
+		var post = getPostFromCalDay(elem);
 		if ( post.length == 0 ) return false;
 		var offset = post.offset().top - topmargin;
-		if ( !duration ) duration = 'slow';
 		jQuery('body').animate({scrollTop:offset},duration);
 	};
 
 	// scroll to next active event
-	scrollTo(jQuery('#today > a'),'fast');
+	scrollTo(jQuery('#today'),0);
 
-	scrollToMonth = function(curCal){
-		var post_day = jQuery(curCal).find('td.ec3_postday').first().children('a');
-		if ( post_day.length != 0 )
-			scrollTo(post_day);
+	getEventDay = function(curCal,first){
+		var days = jQuery(curCal).find('td.ec3_eventday');
+		if ( days.length == 0 ) return jQuery();
+		return (first?days.first():days.last());
+	}
+
+	scrollToMonth = function(curCal,scroll){
+		if ( scroll === false ) return;
+		scrollTo(getEventDay(curCal,requestNextMonth),scroll);
 	};
 
 	loadNewEvents = function(curCal){
+		// update prev/next cal
+		next_cal = ec3.get_next_cal();
+		next_month_post = getPostFromCalDay(getEventDay(next_cal,true));
+		prev_cal = ec3.get_prev_cal();
+		prev_month_post = getPostFromCalDay(getEventDay(prev_cal,false));
 		if ( reload ){
 			postreq++;
 	       	 	jQuery.get(newMonthHref, function(data){
@@ -78,10 +97,10 @@ jQuery(document).ready(function(){
 					jQuery('#event-listing').prepend(content);
 				}
 				filterPosts(curCat);
-				scrollToMonth(curCal);
+				scrollToMonth(curCal,scroll);
 				postreq--;
 			});
-		}else scrollToMonth(curCal);
+		}else scrollToMonth(curCal,scroll);
 	};
 
 	// get the events of next/prev month
@@ -93,20 +112,20 @@ jQuery(document).ready(function(){
 		newMonthHref = jQuery(this).attr('href');
 		var id = jQuery(this).attr('id');
 		requestNextMonth = ( id == 'ec3_next' );
+		scroll = true;
 		if ( requestNextMonth ){
-			reload = !ec3.get_next_cal();
+			reload = !next_cal;
 			ec3.go_next(loadNewEvents);
 		}else{
-			reload = !ec3.get_prev_cal();
+			reload = !prev_cal;
 			ec3.go_prev(loadNewEvents);
 		}
-
 		return false;
 	}); 
 
 	// scroll to corrsponding post when click on day 
 	jQuery('td.ec3_postday > a').live('click',function(){
-		scrollTo(jQuery(this));
+		scrollTo(jQuery(this).parent());
 		return false;
 	});
 
@@ -126,6 +145,47 @@ jQuery(document).ready(function(){
 	jQuery('#wp-calendar caption > a').live('click',function(){
 		scrollToMonth(jQuery(this).closest('table'));
 		return false;
+	});
+
+	// update calendar when scrolling through events
+	jQuery(document).scroll(function(e){
+		var elem = jQuery(window);
+		var obj = jQuery('body');
+
+		if ( next_month_post.length != 0 ) {
+			if ( next_month_post.offset().top-topmargin < obj.scrollTop() ){
+				scroll = false;
+				reload = false;
+				// load next cal month 
+				ec3.go_next(loadNewEvents);
+			}
+		}
+
+		if ( prev_month_post.length != 0 ) {
+			// only go to prev month if there was no reload before
+			if ( prev_month_post.offset().top-topmargin > obj.scrollTop() && !reload ||
+			     prev_month_post.offset().top-(elem.height()-200) > obj.scrollTop() ) {
+				scroll = false;
+				reload = false;
+				// load prev cal month 
+				ec3.go_prev(loadNewEvents);
+			}
+		}
+
+ 		if (jQuery(document).height() <= (obj.scrollTop() + elem.height())) {
+			newMonthHref = jQuery('#ec3_next:last').attr('href');
+			requestNextMonth = true;
+			scroll = false;
+			reload = true;
+			ec3.go_next(loadNewEvents);
+		}else if (obj.scrollTop() == 0 ){
+			newMonthHref = jQuery('#ec3_prev:first').attr('href');
+			requestNextMonth = false;
+			scroll = 0;
+			reload = true;
+			ec3.go_prev(loadNewEvents);
+		} 
+
 	});
 });
 
