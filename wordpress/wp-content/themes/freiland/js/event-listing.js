f819d46461d9bbd2dd859f1d3daeb247b30bc529
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-05-26 13:26:21 +0200

    event-listing.js: fix scope issues with ie
    Conflicts:
    
    	wordpress/wp-content/themes/freiland/js/event-listing.js

--- wordpress/wp-content/themes/freiland/js/event-listing.js
+++ wordpress/wp-content/themes/freiland/js/event-listing.js
@@ -1,16 +1,50 @@
+function event_listing(){
 
-jQuery(document).ready(function(){
-	var topmargin = jQuery('#event-listing > div.post:first').offset().top;
+	var topmargin;
 	// count ajax requests for posts
 	var postreq = 0;
-	var next_href = jQuery('#ec3_next:first').attr('href');
-	var prev_href = jQuery('#ec3_prev:first').attr('href');
+	var next_href;
+	var prev_href;
 	var curPost;
+	var curCat,topCat;
+	var pageLoaded = true;
 	var updateCal = true;
+	// all elements that need to be shown/hidden when loading one event
+	var listingElements = '#mainpost-banner,#event-listing,#wp-calendar,#eventtypes';
+	// all event title links
+	var eventSelector = '#event-listing h2.entry-title > a';
+	// all subcategory links
+	var subcatSelector = '#eventtypes > ul > li > a,ul.children > li > a,ul.event-subcat > li > a';
+	// next/prev post
+	var nextPrevSelector = '.nav-previous > a,.nav-next > a';
+	var baseURL;
+
 
 	var debug = function(text){
 		jQuery('#topright > span').append(text+"</br>");
 	};
+
+	function init(){
+		topmargin = jQuery('#event-listing > div.post:first').offset().top;
+		next_href = jQuery('#ec3_next:first').attr('href');
+		prev_href = jQuery('#ec3_prev:first').attr('href');
+
+		// initialize current cat from 'events' menu item
+		curCat = event_listing.getCatId(jQuery('.current-menu-item'));
+		// the top-level category, this never changes
+		topCat = curCat;
+		// set 'current-cat' class
+		jQuery('#eventtypes ul li.cat-item-'+curCat).addClass('current-cat');
+
+		// the post which is currently on top
+		curPost = event_listing.getPostFromCalDay(jQuery('#today'));
+
+		// when loading the page, scroll to next active event
+		event_listing.scrollToPost(curPost,0);
+		// on page load, check for update
+		event_listing.checkForUpdate();
+	};
+	event_listing.init = init;
 
 	// filter posts for given category
 	// show 404 if no posts found
@@ -57,23 +91,17 @@ jQuery(document).ready(function(){
 	};
 
 	// return the categroy id of this element
-	var getCatId = function(elem){
+	function getCatId(elem){
 		var cat = elem.attr('class');
     		var xCat=new RegExp('.*cat-item-([0-9]+)');
 		var catid=xCat.exec(cat);
 		if(!catid) return null;
 		return catid[1];
 	};
-
-	// initialize current cat from 'events' menu item
-	var curCat = getCatId(jQuery('.current-menu-item'));
-	// the top-level category, this never changes
-	var topCat = curCat;
-	// set 'current-cat' class
-	jQuery('#eventtypes ul li.cat-item-'+curCat).addClass('current-cat');
+	event_listing.getCatId = getCatId;
 
 	// get the next event following the given cal day
-	var getPostFromCalDay = function(elem){
+	function getPostFromCalDay(elem){
 		var eventDay = jQuery();
 		var curDay = elem;
 		while ( eventDay.length == 0 && curDay.length!=0 ){
@@ -91,9 +119,10 @@ jQuery(document).ready(function(){
 		id = parseInt(id[0],10);
 		return jQuery('#post-'+id);
 	};
+	event_listing.getPostFromCalDay = getPostFromCalDay;
 
 	// scroll to given post
-	var scrollToPost = function(post,duration,callback){
+	function scrollToPost(post,duration,callback){
 		if ( post.length == 0 || duration === false ){
 			if ( callback ) callback();
 			return ;
@@ -101,11 +130,13 @@ jQuery(document).ready(function(){
 		var offset = post.offset().top - (topmargin);
 		jQuery('html,body').animate({scrollTop:offset},duration,callback);
 	};
+	event_listing.scrollToPost = scrollToPost;
 
 	// scroll to next post following the given cal day
-	var scrollToCalDay = function(elem,duration,callback){
+	function scrollToCalDay(elem,duration,callback){
 		scrollToPost(getPostFromCalDay(elem),duration,callback);
 	};
+	event_listing.scrollToCalDay = scrollToCalDay;
 
 	// return the first/last event day from given calendar
 	getEventDay = function(curCal,first){
@@ -113,19 +144,13 @@ jQuery(document).ready(function(){
 		if ( days.length == 0 ) return jQuery();
 		return (first?days.first():days.last());
 	}
-	// the post which is currently on top
-	var curPost = getPostFromCalDay(jQuery('#today'));
-	// page was just loaded;
-	var pageLoaded = true;
-
-	// when loading the page, scroll to next active event
-	scrollToPost(curPost,0);
 
 	// scroll to first/last event in month shown by the given calendar
-	scrollToMonth = function(curCal,first,scroll,callback){
+	function scrollToMonth(curCal,first,scroll,callback){
 		if ( scroll === false ) return;
 		scrollToCalDay(getEventDay(curCal,first),scroll,callback);
 	};
+	event_listing.scrollToMonth = scrollToMonth;
 
 	// load new events with ajax
 	// apend/prepend them to the list and filter them
@@ -153,13 +178,14 @@ jQuery(document).ready(function(){
 	};
 
 	// parse a calendar month url into [begin,year,month,rest]
-	parseHref = function(href){
+	function parseHref(href){
 	 	var reg = /(.+m=)(\d\d\d\d)(\d\d)(.+)/;
 		reg.exec(href);
 		var month = parseInt(RegExp.$3,10);
 		var year = parseInt(RegExp.$2,10);
 		return [RegExp.$1, year, month, RegExp.$4];
 	}
+	event_listing.parseHref = parseHref;
 
 	// test if the month of href is between prev_href and next_href
 	hrefLoaded = function(href){
@@ -172,16 +198,13 @@ jQuery(document).ready(function(){
 		return ( hrefDate > pDate && hrefDate < nDate);
 	}
 
-	// get the events of next/prev month
-	// and add them to the event listing
-	jQuery('#wp-calendar #next > a,' + 
-	       '#wp-calendar #prev > a,').live('click',function(){
+	function nextPrevClicked(elem){
 		if ( postreq > 0 ) return false;
 
-		var id = jQuery(this).attr('id');
+		var id = elem.attr('id');
 		var requestNextMonth = ( id == 'ec3_next' );
 		var fun = null;
-		var reload = !hrefLoaded((requestNextMonth?next_href:prev_href));
+		var reload = !hrefLoaded(elem.attr('href'));
 		if ( requestNextMonth ){
 			fun = ec3.go_next;
 		}else{
@@ -199,24 +222,8 @@ jQuery(document).ready(function(){
 				updateCal = true;
 			});
 		});
-		return false;
-	}); 
-
-	// scroll to corrsponding post when click on day 
-	jQuery('td.ec3_postday > a').live('click',function(){
-		scrollToCalDay(jQuery(this).parent());
-		return false;
-	});
-
-	// all elements that need to be shown/hidden when loading one event
-	var listingElements = '#mainpost-banner,#event-listing,#wp-calendar,#eventtypes';
-	// all event title links
-	var eventSelector = '#event-listing h2.entry-title > a';
-	// all subcategory links
-	var subcatSelector = '#eventtypes > ul > li > a,ul.children > li > a,ul.event-subcat > li > a';
-	// next/prev post
-	var nextPrevSelector = '.nav-previous > a,.nav-next > a';
-
+	};
+	event_listing.nextPrevClicked = nextPrevClicked;
 
 	// filter posts when clicking on event sub categories
 	subcatClicked = function(newCat){
@@ -249,8 +256,7 @@ jQuery(document).ready(function(){
 	// jquery - address plugin
 	// this provides browser history for ajax links
 	// used for subcategory and post links
-	var baseURL;
-	jQuery.address.init(function(event) {
+	function initAddress(){
                 // Initializes the plugin
 		baseURL = jQuery.address.baseURL().replace('events','');
 		jQuery(eventSelector + ',' + 
@@ -258,14 +264,15 @@ jQuery(document).ready(function(){
 		       nextPrevSelector).address(function() {
 			return jQuery(this).attr('href').replace(baseURL,'');
 		});
-	}).change(function(event) {
+	};
+	event_listing.initAddress = initAddress;
+
+	function addressChanged(event){
 		var orgvalue = event.value.replace(/^\//, '');
 		var value = baseURL + event.value.replace(/^\//, '');
 
 		// load event listing
 		if ( orgvalue == '' ){
-			// XXX: workaround to prevent scrolling to 0
-			pageLoaded = true;
 			subcatClicked(topCat);
 			return;
 		}
@@ -294,13 +301,8 @@ jQuery(document).ready(function(){
 				});
 			} 
 		});
-	});
-
-	// scroll to first of month when clicking on month link
-	jQuery('#wp-calendar caption > a').live('click',function(){
-		scrollToMonth(jQuery(this).closest('table'),true);
-		return false;
-	});
+	};
+	event_listing.addressChanged = addressChanged;
 
 	// return the url pointing to the previous month of given url
 	decrementHref = function(href){
@@ -368,9 +370,9 @@ jQuery(document).ready(function(){
 		}
 
 		if ( callback ) callback();
-	}
+	};
 
-	var checkForUpdate = function(){
+	function checkForUpdate(){
 		var elem = jQuery(window);
 		var variance = 5;
 		// scroll reached the bottom
@@ -385,11 +387,10 @@ jQuery(document).ready(function(){
 			});
 		}
 	};
-	// on page load, check for update
-	checkForUpdate();
+	event_listing.checkForUpdate = checkForUpdate;
 
 	// update calendar and load new events when scrolling through event list
-	jQuery(document).scroll(function(e){
+	function onScrolled(){
 		var elem = jQuery(window);
 		// XXX: workaround to prevent scrolling to top on ajax page reload
 		if ( pageLoaded ) {
@@ -407,6 +408,44 @@ jQuery(document).ready(function(){
 
 //		debug('curPost:'+curPost.find('a').attr('title')+':updateCal:'+updateCal);
 		checkForUpdate();
+	};
+	event_listing.onScrolled = onScrolled;
+}
+
+event_listing();
+jQuery(document).ready(function(){
+	event_listing.init();	
+
+	// get the events of next/prev month
+	// and add them to the event listing
+	jQuery('#wp-calendar #next > a,' + 
+	       '#wp-calendar #prev > a,').live('click',function(){
+		event_listing.nextPrevClicked(jQuery(this));
+		return false;
+	}); 
+
+	// scroll to corrsponding post when click on day 
+	jQuery('td.ec3_postday > a').live('click',function(){
+		event_listing.scrollToCalDay(jQuery(this).parent());
+		return false;
 	});
+
+	// update calendar and load new events when scrolling through event list
+	jQuery(document).scroll(function(e){
+		event_listing.onScrolled();
+	});
+
+	// scroll to first of month when clicking on month link
+	jQuery('#wp-calendar caption > a').live('click',function(){
+		event_listing.scrollToMonth(jQuery(this).closest('table'),true);
+		return false;
+	});
+
+	jQuery.address.init(function(event) {
+		event_listing.initAddress();
+	}).change(function(event) {
+		event_listing.addressChanged(event);
+	});
+
 });
 
