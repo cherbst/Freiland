b844e2c4a5a9861806fd33f10be15800d2a317e2
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-06-04 00:34:54 +0200

    hidden-scroll: use css top for scrolling

--- wordpress/wp-content/themes/freiland/js/hidden-scroll.js
+++ wordpress/wp-content/themes/freiland/js/hidden-scroll.js
@@ -1,36 +1,36 @@
 function innerScroll(elem){
 	var ontop = false;
 	var onbottom = false;
-	var inittop = elem.offset().top;
 
+	elem.css('position','relative');
+	elem.css('top',0);
 	elem.mousewheel(function(event, delta) {
-		var cur = elem.offset();
 		var v = 50;
 		var variance = 100;
 		var ret = true;
 		var triggerTop = !ontop;
 		var triggerBottom = !onbottom;
-		var newTop = cur.top+(delta*v);
-		var minTop = 
-		    jQuery('#container').offset().top + jQuery('#container').height() - elem.height() - variance;
+		var curTop = parseInt(elem.css('top'),10);
+		var newTop = curTop+(delta*v);
+		var minTop = - elem.height() + elem.parent().height()/2;
 
 		if ( delta < 0 )
 			newTop = Math.max(newTop,minTop);
 		else
-			newTop = Math.min(newTop,inittop);
+			newTop = Math.min(newTop,0);
 
 		if( newTop <= minTop && delta < 0){
 			onbottom = true;
 			ontop = false;
-		}else if (newTop >= inittop && delta > 0 ){
+		}else if (newTop >= 0 && delta > 0 ){
 			ontop = true;
 			onbottom = false;
 		}else{
 			ontop = onbottom = false;
 			ret = false;
 		}
-		if ( newTop < cur.top && delta < 0 || newTop > cur.top && delta > 0 )
-			elem.offset({top:newTop,left:cur.left});
+		if ( newTop < curTop && delta < 0 || newTop > curTop && delta > 0 )
+			elem.css('top',newTop);
 		if ( triggerTop || triggerBottom ){
 			elem.trigger('hiddenscroll',[triggerTop && ontop,triggerBottom && onbottom]);
 		}
