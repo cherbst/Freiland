2795104d7421cd52155a7f5fda417a883ddea05c
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-06-07 17:44:20 +0200

    freiland/land: set links to house pages

--- wordpress/wp-content/themes/freiland/loop-land.php
+++ wordpress/wp-content/themes/freiland/loop-land.php
@@ -47,17 +47,17 @@
 				<img class="alignleft" usemap="#map" src="<?php echo $url; ?>lageplan.png" alt="" />
 				<map name="map">
 
-				<area onmouseover="TagToTip('land_bba')" onmouseout="UnTip()" shape="poly" 
+				<area href="haus1" onmouseover="TagToTip('land_bba')" onmouseout="UnTip()" shape="poly" 
 					coords="60,280,200,210,220,250,80,320" href="#" alt="<?php echo $bba; ?>" />
-				<area onmouseover="TagToTip('land_cafe')" onmouseout="UnTip()" shape="rectangle" 
+				<area href="haus2" onmouseover="TagToTip('land_cafe')" onmouseout="UnTip()" shape="rectangle" 
 					coords="200,410,230,490" href="#" alt="<?php echo $cafe; ?>" />
-				<area onmouseover="TagToTip('land_spartacus')" onmouseout="UnTip()" shape="rectangle" 
-					coords="85,380,115,485" href="#" alt="<?php echo $spartacus; ?>" />
-				<area onmouseover="TagToTip('land_jfb')" onmouseout="UnTip()" shape="rectangle" 
+				<area href="haus3" onmouseover="TagToTip('land_spartacus')" onmouseout="UnTip()" shape="rectangle" 
+					coords="135,380,170,485" href="#" alt="<?php echo $spartacus; ?>" />
+				<area href="haus4" onmouseover="TagToTip('land_jfb')" onmouseout="UnTip()" shape="rectangle" 
 					coords="80,375,100,510" href="#" alt="<?php echo $jfb; ?>" />
-				<area onmouseover="TagToTip('land_wk')" onmouseout="UnTip()" shape="rectangle" 
+				<area href="haus5" onmouseover="TagToTip('land_wk')" onmouseout="UnTip()" shape="rectangle" 
 					coords="175,515,225,550" href="#" alt="<?php echo $wk; ?>" />
-				<area onmouseover="TagToTip('land_aquarium')" onmouseout="UnTip()" shape="rectangle" 
+				<area href="haus6" onmouseover="TagToTip('land_aquarium')" onmouseout="UnTip()" shape="rectangle" 
 					coords="220,215,240,225" href="#" alt="<?php echo $aquarium; ?>" />
 				</map>
 
