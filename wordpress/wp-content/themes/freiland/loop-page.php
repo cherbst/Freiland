60a44bcb4733398e8cb5d49fbd01a363eac527bb
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-06-07 18:31:20 +0200

    freiland: layout land pages

--- wordpress/wp-content/themes/freiland/loop-page.php
+++ wordpress/wp-content/themes/freiland/loop-page.php
@@ -18,18 +18,17 @@
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php 
+						if ( has_post_thumbnail() ) { 
+							the_post_thumbnail('full');
+						} 
+					?>
 					<?php if ( is_front_page() ) { ?>
 						<h2 class="entry-title"><?php the_title(); ?></h2>
 					<?php } else { ?>
 						<h1 class="entry-title"><?php the_title(); ?></h1>
 					<?php } ?>
-
 					<div class="entry-content">
-						<?php 
-							if ( has_post_thumbnail() ) { 
-								the_post_thumbnail('full');
-							} 
-						?>
 						<?php the_content(); ?>
 						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
 						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
