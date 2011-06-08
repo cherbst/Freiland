7aea50949ad3af065e1d05d8f656433d0ad72a01
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-06-08 16:03:48 +0200

    freiland/land: make legend clickable

--- wordpress/wp-content/themes/freiland/loop-land.php
+++ wordpress/wp-content/themes/freiland/loop-land.php
@@ -16,12 +16,19 @@
 			<?php 
 				$url = dirname(get_bloginfo('stylesheet_url')).'/images/';
 
-				$coords = array( 'haus1' => array( 'poly','60,280,200,210,220,250,80,320'), 
-						 'haus2' => array( 'rectangle','200,410,230,490'), 
-						 'haus3' => array( 'rectangle','135,380,170,485'), 
-						 'haus4' => array( 'rectangle','80,375,100,510'), 
-						 'haus5' => array( 'rectangle','175,515,225,550'), 
-						 'haus6' => array( 'rectangle','220,215,240,225'));
+				$coords = array( 'haus1' => array( array( 'poly','60,280,200,210,220,250,80,320'), 
+								   array('0,0,200,10') ),
+						 'haus2' => array( array( 'rectangle','200,410,230,490'), 
+								   array('0,25,200,35') ),
+						 'haus3' => array( array( 'rectangle','135,380,170,485'), 
+								   array('0,50,200,60') ),
+						 'haus4' => array( array( 'rectangle','80,375,100,510'), 
+								   array('0,75,200,85') ),
+						 'haus5' => array( array( 'rectangle','175,515,225,550'), 
+								   array('0,100,200,110') ),
+						 'haus6' => array( array( 'rectangle','220,215,240,225'),
+								   array('0,140,200,150') )
+						);
 
 				$arr = array('post_parent' => $post->ID,
 					     'post_type' => 'page',
@@ -50,14 +57,26 @@
 				?>
 				<area href="<?php echo $house; ?>" 
 				      onmouseover="TagToTip('land_<?php echo $house; ?>')" onmouseout="UnTip()" 
-				      shape="<?php echo $coords[$house][0]; ?>" 
-				      coords="<?php echo $coords[$house][1]; ?>" 
+				      shape="<?php echo $coords[$house][0][0]; ?>" 
+				      coords="<?php echo $coords[$house][0][1]; ?>" 
 				      alt="<?php echo $page->post_title; ?>" />
 				<?php } ?>
 				</map>
 
 				<div id="legende_box">
-					<img src="<?php echo $url; ?>lageplan_beschreibung.png" alt="" />
+					<img usemap="#map1" src="<?php echo $url; ?>lageplan_beschreibung.png" alt="" />
+					<map name="map1">
+
+					<?php 
+						foreach ( $houses as $house => $page){
+							if ( !isset($coords[$house]) ) continue;
+					?>
+						<area href="<?php echo $house; ?>"
+				      		      shape="rectangle" 
+						      coords="<?php echo $coords[$house][1][0]; ?>" 
+						      alt="<?php echo $page->post_title; ?>" />
+					<?php } ?>
+					</map>
 				</div>
 			</div>
 		</div><!-- .entry-content -->
