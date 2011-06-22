8b99eef7a9586f659ca421346248fb4a5b4a69a6
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-06-22 14:40:01 +0200

    freiland/sponsor-images.js: respect min-width

--- wordpress/wp-content/themes/freiland/js/sponsor-images.js
+++ wordpress/wp-content/themes/freiland/js/sponsor-images.js
@@ -5,9 +5,16 @@ jQuery(document).ready(function(){
 		var paddingRight = (parseInt(jQuery(imgs).first().css('padding-right'),10) || 0 )
 		var paddingLeft =  (parseInt(jQuery(imgs).first().css('padding-left'),10) || 0 );
 		var maxWidth =  (parseInt(jQuery(imgs).first().css('max-width'),10) || 0 );
+		var minWidth =  (parseInt(jQuery(imgs).first().css('min-width'),10) || 0 );
 		var padding = paddingLeft + paddingRight;
-		var newWidth = (jQuery('div.sponsor').width()-padding*(num-1))/num;
-		if ( maxWidth > 0 ) newWidth = Math.min(newWidth,maxWidth); 
+		var newWidth;
+		var availableWidth = jQuery('div.sponsor').width();
+
+		do{
+			newWidth = (availableWidth-padding*(num-1))/num;
+			if ( maxWidth > 0 ) newWidth = Math.min(newWidth,maxWidth); 
+			availableWidth *= 2;
+		}while( newWidth < minWidth );
 
 		jQuery(imgs).css('width',newWidth);
 		if ( paddingLeft > 0 )
