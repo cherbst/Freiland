2cb054eb2dba9c428c0f5a76a430f59da3a42ff0
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-07-06 11:56:20 +0200

    freiland: add better 404 text to gallery page

--- wordpress/wp-content/themes/freiland/loop.php
+++ wordpress/wp-content/themes/freiland/loop.php
@@ -30,11 +30,19 @@
 <?php /* If there are no posts to display, such as an empty archive page */ ?>
 <?php if ( ! have_posts() ) : ?>
 	<div id="post-0" class="post error404 not-found">
+	<?php global $wp_query;
+		if ( $wp_query->query_vars['category_name'] == 'images'){ ?>
+		<h1 class="entry-title">Noch keine Galerieen<br /> vorhanden</h1>
+		<div class="entry-content">
+			<p><?php _e( 'An dieser Stelle werden in Kürze Bildergalerien hinzugefügt.', 'twentyten' ); ?></p>
+		</div><!-- .entry-content -->
+		<?php }else{ ?>
 		<h1 class="entry-title"><?php _e( 'Not Found', 'twentyten' ); ?></h1>
 		<div class="entry-content">
 			<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyten' ); ?></p>
 			<?php get_search_form(); ?>
 		</div><!-- .entry-content -->
+		<?php } ?>
 	</div><!-- #post-0 -->
 <?php endif; ?>
 
