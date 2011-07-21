cherbst/Freiland

commit 42fa4ab4369ed91a04ea3f02e30c9b5f5c833d41

evl.js: do not filter posts on page load

wordpress/wp-content/themes/freiland/js/event-listing.js
@@ -266,9 +266,6 @@ function event_listing(){
 		var hidden,shown, toShow;
 		var duration = 1000;
 
-		if ( firstCall )
-			duration = 0;
-
 		// keep all posts matching filter
 		hidden = allPosts.not(filter);
 		shown = allPosts.filter(filter);
@@ -279,10 +276,7 @@ function event_listing(){
 		var notfound = jQuery('.error404');
 
 		if ( postsToShow ){
-			if ( firstCall )
-				firstCall = false;
-			else
-				curPost = getNewCurPost(filter,curPost);
+			curPost = getNewCurPost(filter,curPost);
 			computeNewHeights(allPosts,toHide,toShow,hidden,shown);
 
 			toHide.each(function(){
@@ -691,7 +685,11 @@ function event_listing(){
 
 		// load event listing
 		if ( orgvalue == '' ){
-			subcatClicked(topCat);
+			if ( firstCall ){
+				scrollToPost(curPost,0);
+				firstCall = false;
+			}else
+				subcatClicked(topCat);
 			return;
 		}
 

commit 7c45ee506cde6e6789949534679cf7215f00640d

evl.js: fix scroll area broken in 42fa4ab4369ed91a

wordpress/wp-content/themes/freiland/js/event-listing.js
@@ -68,6 +68,8 @@ function event_listing(){
 		if ( curPost.length == 0 )
 			curPost = jQuery('#event-listing > div > div.post').first();
 
+		// set scrollable to top
+		innerScroll.setScrollableToTop(true,event_listing.getLastPostHeight());
 		// preload prev and next month
 		event_listing.loadEvents(prev_href);
 		event_listing.loadEvents(next_href);
@@ -109,12 +111,14 @@ function event_listing(){
 		});
 	}
 
+	// return the height of the last post + padding
 	function getLastPostHeight(){
 		var lastPost = jQuery('#event-listing > div > div.post').filter(':visible').last();
 		var padding = (parseInt(lastPost.css('padding-top'),10) || 0)
 			    + (parseInt(lastPost.css('padding-bottom'),10) || 0);
 		return lastPost.height() + padding;
 	};
+	event_listing.getLastPostHeight = getLastPostHeight;
 
 	function getNewCurPost(filter,oldCur){
 		var newCur = oldCur;