21e61961a1b09f74970b6397cda02f4b1fe38ef9
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-07-26 11:15:49 +0200

    freiland: fix land pages for ie6

--- wordpress/wp-content/themes/freiland/loop-page.php
+++ wordpress/wp-content/themes/freiland/loop-page.php
@@ -23,16 +23,18 @@
 							the_post_thumbnail('full');
 						} 
 					?>
-					<?php if ( is_front_page() ) { ?>
-						<h2 class="entry-title"><?php the_title(); ?></h2>
-					<?php } else { ?>
-						<h1 class="entry-title"><?php the_title(); ?></h1>
-					<?php } ?>
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
-						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
-					</div><!-- .entry-content -->
+					<div class="page-content">
+						<?php if ( is_front_page() ) { ?>
+							<h2 class="entry-title"><?php the_title(); ?></h2>
+						<?php } else { ?>
+							<h1 class="entry-title"><?php the_title(); ?></h1>
+						<?php } ?>
+						<div class="entry-content">
+							<?php the_content(); ?>
+							<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+							<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
+						</div><!-- .entry-content -->
+					</div><!-- .page-content -->
 				</div><!-- #post-## -->
 
 				<?php comments_template( '', true ); ?>
