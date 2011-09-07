df2fa792538f229ad071c0812f13476b2f527a66
Christoph Herbst <chris.p.herbst@googlemail.com>
2011-09-07 22:27:50 +0200

    freiland: use seperate css for welcome page

--- wordpress/wp-content/themes/freiland/welcome.php
+++ wordpress/wp-content/themes/freiland/welcome.php
@@ -23,34 +23,11 @@ Template Name: Welcome
 	if ( $site_description && ( is_home() || is_front_page() ) )
 		echo " | $site_description";
 
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
-
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo dirname(get_bloginfo( 'stylesheet_url' )); ?>/welcome.css" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <link rel="shortcut icon" href="<?php echo dirname(get_bloginfo('stylesheet_url')).'/images'; ?>/favicon.ico" type="image/x-icon" />
-<?php
-	/* We add some JavaScript to pages with the comment form
-	 * to support sites with threaded comments (when in use).
-	 */
-	if ( is_singular() && get_option( 'thread_comments' ) )
-		wp_enqueue_script( 'comment-reply' );
-
-	/* Always have wp_head() just before the closing </head>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to add elements to <head> such
-	 * as styles, scripts, and meta tags.
-	 */
-	wp_head();
-	
-?>
-<!--[if lte IE 6]>
-	<script type="text/javascript" src="<?php echo dirname(get_bloginfo('stylesheet_url')) . '/js/supersleight.plugin.js';?>"></script>
-	<script type="text/javascript" src="<?php echo dirname(get_bloginfo('stylesheet_url')) . '/js/ie6-fixes.js';?>"></script>
-<![endif]-->
 </head>
 
 <body <?php body_class(); ?>>
